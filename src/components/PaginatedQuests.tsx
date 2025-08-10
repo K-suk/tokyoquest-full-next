cab5ccb3164c68ed2b2ce6db118b06d3a092408c
K-suk <kuma.kou.0201@gmail.com>
2025-08-10 19:48:07 +0900

    update coloring

--- src/components/PaginatedQuests.tsx
+++ src/components/PaginatedQuests.tsx
@@ -96,7 +96,7 @@ export default function PaginatedQuests({
                                                 onClick={() => handlePageChange(pageNum)}
                                                 disabled={loading}
                                                 className={`px-3 py-2 rounded-lg border ${currentPage === pageNum
-                                                    ? "bg-blue-500 text-white border-blue-500"
+                                                    ? "bg-[#ff5757] text-white border-[#ff5757]"
                                                     : "bg-white text-gray-700 border-gray-300 hover:bg-gray-50"
                                                     }`}
                                             >
